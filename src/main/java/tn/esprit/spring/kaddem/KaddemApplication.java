029c9f66af01f6264e1ad4a997db805cf3f16a2c
Houda <chambihouda@gmail.com>
2025-03-13 10:48:30 +0100

    module etudiant

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class KaddemApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
