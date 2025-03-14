e467638c9d38fd775d576b69b5d46214f5dc4994
Houda <chambihouda@gmail.com>
2025-03-14 04:51:59 +0100

    ajout nexus

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.kaddem;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
